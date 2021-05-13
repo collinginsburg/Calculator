30b4cfd387cfa03a196b89bc1877dc55285515f4
collinginsburg <chginsburg@email.wm.edu>
2021-05-13 15:25:40 -0700

    Got the calculate add function to work well. Has two implicit options: add onto the previous sum clicking plus again and then whichever number you want to add to it. And then a new operation options which resets the calc if you press any number

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
 // ******************calc FUNCTIONS**************************************
 
-function subtraction (a,b){
-    return a-b;
+function subtract (firstNumber,secondNumber){
+    return firstNumber-secondNumber;
 }
-function add(a, b) {
-    return a+b;
+function add(firstNumber, secondNumber) {
+    return firstNumber+secondNumber;
 }
-function multiply(a,b){
-      return a*b;
+function multiply(firstNumber,secondNumber){
+      return firstNumber*secondNumber;
 }
-function divide(a, b) {
-    return a/b;
+function divide(firstNumber, secondNumber) {
+    return firstNumber/secondNumber;
 }
-
-function operate(operation,a,b){
-
-    if(operation === "add"){
-        return add(a,b);
-    }
-    if(operation === "subtraction"){
-        return subtraction(a,b);
-    }
-    if(operation === "multiply"){
-        return multiply(a,b);
-    }
-    if(operation === "divide"){
-        return divide(a,b);
-    }
-
-}
-
 
 // ******************button FUNCTIONS**************************************
 const SCREEN =  document.querySelector('.screen');
@@ -46,94 +28,255 @@ function updateDisplay(){
 const ZERO = document.querySelector('.btn.zero');
 ZERO.addEventListener('click', displayZero);
 function displayZero(){
-    SCREEN.textContent += '0';
-    updateDisplay();
-    console.log(displayvalue);
+        SCREEN.textContent += '0';
 }
 
 const PERIOD = document.querySelector('.btn.period');
 PERIOD.addEventListener('click', displayPeriod);
 function displayPeriod(){
     SCREEN.textContent += '.';
-    updateDisplay();
-    console.log(displayvalue);
 }
 
 const ONE = document.querySelector('.btn.one');
 ONE.addEventListener('click', displayOne);
 function displayOne(){
     SCREEN.textContent += '1';
-    updateDisplay();
-    console.log(displayvalue);
 }
 const TWO = document.querySelector('.btn.two');
 TWO.addEventListener('click', displayTwo);
 function displayTwo(){
     SCREEN.textContent += '2';
-    updateDisplay();
-    console.log(displayvalue);
 }
 const THREE = document.querySelector('.btn.three');
 THREE.addEventListener('click', displayThree);
 function displayThree(){
     SCREEN.textContent += '3';
-    updateDisplay();
-    console.log(displayvalue);
 }
 const FOUR = document.querySelector('.btn.four');
 FOUR.addEventListener('click', displayFour);
 function displayFour(){
     SCREEN.textContent += '4';
-    updateDisplay();
-    console.log(displayvalue);
 }
 const FIVE = document.querySelector('.btn.five');
 FIVE.addEventListener('click', displayFive);
 function displayFive(){
     SCREEN.textContent += '5';
-    updateDisplay();
-    console.log(displayvalue);
 }
 const SIX = document.querySelector('.btn.six');
 SIX.addEventListener('click', displaySix);
 function displaySix(){
     SCREEN.textContent += '6';
-    updateDisplay();
-    console.log(displayvalue);
 }
 const SEVEN = document.querySelector('.btn.seven');
 SEVEN.addEventListener('click', displaySeven);
 function displaySeven(){
     SCREEN.textContent += '7';
-    updateDisplay();
-    console.log(displayvalue);
 }
 const EIGHT = document.querySelector('.btn.eight');
 EIGHT.addEventListener('click', displayEight);
 function displayEight(){
     SCREEN.textContent += '8';
-    updateDisplay();
-    console.log(displayvalue);
 }
 const NINE = document.querySelector('.btn.nine');
 NINE.addEventListener('click', displayNine);
 function displayNine(){
     SCREEN.textContent += '9';
-    updateDisplay();
-    console.log(displayvalue);
 }
 
 
 
 const CLEAR = document.querySelector('.btn.clear');
-CLEAR.addEventListener('click', clearDisplay);
-function clearDisplay(){
+CLEAR.addEventListener('click', clearDisplayReset);
+function clearDisplayReset(){
     SCREEN.textContent = '';
-    updateDisplay();
-    console.log(displayvalue);
+    firstNumberOn = "n/a";
+    secondNumberOn = "n/a";
+    additionOn = false;
+    subractionOn = false;
+    multiplicationOn = false;
+    divisionOn = false;
+    addNumberButtonFunctionality();
+    removeAfterOperateButtonFunctionality();
 };
+function prepCalcForNextOperation(){
+    firstNumber = result;
+    SCREEN.textContent = ''; 
+    firstNumberOn = "yes";
+}
+
+// ******************operator button FUNCTIONS**************************************
+let additionOn = false;
+let subractionOn = false;
+let multiplicationOn = false;
+let divisionOn = false;
+let firstNumber;
+let secondNumber;
+let firstNumberOn = "n/a";
+let secondNumberOn = "n/a";
+let result;
+
+function operate(){
+
+    if(additionOn === true){
+        secondNumber = Number(SCREEN.textContent);
+        result = add(firstNumber,secondNumber);
+        SCREEN.textContent = `= ${result}`;
+        firstNumber = result;
+        firstNumberOn = "yes"
+        secondNumberOn = "no"
+        additionOn = false;
+        removeNumberButtonFunctionality();
+        addAfterOperateButtonFunctionality();
+
+    }
+    if(subtractionOn === true){
+        return subtract(firstNumber,secondNumber);
+    }
+    if(multiplicationOn === true){
+        return multiply(firstNumber,secondNumber);
+    }
+    if(divisionOn === true){
+        return divide(firstNumber,secondNumber);
+    }
+    firstNumberOn = true;
+
+}
+
+function removeNumberButtonFunctionality(){
+    NINE.removeEventListener('click', displayNine);
+    EIGHT.removeEventListener('click', displayEight);
+    SEVEN.removeEventListener('click', displaySeven);
+    SIX.removeEventListener('click', displaySix);
+    FIVE.removeEventListener('click', displayFive);
+    FOUR.removeEventListener('click', displayFour);
+    THREE.removeEventListener('click', displayThree);
+    TWO.removeEventListener('click', displayTwo);
+    ONE.removeEventListener('click', displayOne);
+    ZERO.removeEventListener('click', displayZero);
+    PERIOD.removeEventListener('click', displayPeriod);
+}
+function addAfterOperateButtonFunctionality(){
+    NINE.addEventListener('click', displayNineAfterOperate);
+    EIGHT.addEventListener('click', displayEightAfterOperate);
+    SEVEN.addEventListener('click', displaySevenAfterOperate);
+    SIX.addEventListener('click', displaySixAfterOperate);
+    FIVE.addEventListener('click', displayFiveAfterOperate);
+    FOUR.addEventListener('click', displayFourAfterOperate);
+    THREE.addEventListener('click', displayThreeAfterOperate);
+    TWO.addEventListener('click', displayTwoAfterOperate);
+    ONE.addEventListener('click', displayOneAfterOperate);
+    ZERO.addEventListener('click', displayZeroAfterOperate);
+    PERIOD.addEventListener('click', displayPeriodAfterOperate);
+}
+function removeAfterOperateButtonFunctionality(){
+    NINE.removeEventListener('click', displayNineAfterOperate);
+    EIGHT.removeEventListener('click', displayEightAfterOperate);
+    SEVEN.removeEventListener('click', displaySevenAfterOperate);
+    SIX.removeEventListener('click', displaySixAfterOperate);
+    FIVE.removeEventListener('click', displayFiveAfterOperate);
+    FOUR.removeEventListener('click', displayFourAfterOperate);
+    THREE.removeEventListener('click', displayThreeAfterOperate);
+    TWO.removeEventListener('click', displayTwoAfterOperate);
+    ONE.removeEventListener('click', displayOneAfterOperate);
+    ZERO.removeEventListener('click', displayZeroAfterOperate);
+    PERIOD.removeEventListener('click', displayPeriodAfterOperate);
+}
+function addNumberButtonFunctionality(){
+    NINE.addEventListener('click', displayNine);
+    EIGHT.addEventListener('click', displayEight);
+    SEVEN.addEventListener('click', displaySeven);
+    SIX.addEventListener('click', displaySix);
+    FIVE.addEventListener('click', displayFive);
+    FOUR.addEventListener('click', displayFour);
+    THREE.addEventListener('click', displayThree);
+    TWO.addEventListener('click', displayTwo);
+    ONE.addEventListener('click', displayOne);
+    ZERO.addEventListener('click', displayZero);
+    PERIOD.addEventListener('click', displayPeriod);
+}
+function displayNineAfterOperate(){
+    clearDisplayReset();
+    SCREEN.textContent += '9';
+    removeAfterOperateButtonFunctionality();
+}
+function displayEightAfterOperate(){
+    clearDisplayReset();
+    SCREEN.textContent += '8';
+    removeAfterOperateButtonFunctionality();
+}
+function displaySevenAfterOperate(){
+    clearDisplayReset();
+    SCREEN.textContent += '7';
+    removeAfterOperateButtonFunctionality();
+}
+function displaySixAfterOperate(){
+    clearDisplayReset();
+    SCREEN.textContent += '6';
+    removeAfterOperateButtonFunctionality();
+}
+function displayFiveAfterOperate(){
+    clearDisplayReset();
+    SCREEN.textContent += '5';
+    removeAfterOperateButtonFunctionality();
+}
+function displayFourAfterOperate(){
+    clearDisplayReset();
+    SCREEN.textContent += '4';
+    removeAfterOperateButtonFunctionality();
+}
+function displayThreeAfterOperate(){
+    clearDisplayReset();
+    SCREEN.textContent += '3';
+    removeAfterOperateButtonFunctionality();
+}
+function displayTwoAfterOperate(){
+    clearDisplayReset();
+    SCREEN.textContent += '2';
+    removeAfterOperateButtonFunctionality();
+}
+function displayOneAfterOperate(){
+    clearDisplayReset();
+    SCREEN.textContent += '1';
+    removeAfterOperateButtonFunctionality();
+}
+function displayZeroAfterOperate(){
+    clearDisplayReset();
+    SCREEN.textContent += '0';
+    removeAfterOperateButtonFunctionality();
+}
+function displayPeriodAfterOperate(){
+    clearDisplayReset();
+    SCREEN.textContent += '.';
+    removeAfterOperateButtonFunctionality();
+}
+
+
 const DIVIDE = document.querySelector('.btn.divide');
 const MULTIPLY= document.querySelector('.btn.multiply');
 const MINUS = document.querySelector('.btn.minus');
+
+
+
 const PLUS = document.querySelector('.btn.plus');
+PLUS.addEventListener('click', additionOperation);
+function additionOperation(){
+    if(secondNumberOn === "no"){ //for continuing operations
+        prepCalcForNextOperation();
+        removeAfterOperateButtonFunctionality();
+        addNumberButtonFunctionality();
+        setAdditionOn();
+    }else{ // for first operations
+        firstNumber = Number(SCREEN.textContent);
+        firstNumberOn = "yes";
+        SCREEN.textContent = '';
+        setAdditionOn();
+    }
+}
+function setAdditionOn(){
+    additionOn = true;
+    subtractionOn = false;
+    multiplicationOn = false;
+    divisionOn = false;
+}
 const EQUALS = document.querySelector('.btn.equals');
+EQUALS.addEventListener('click', operate);
