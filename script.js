e6f5c81ffb7a3019f7a3b9544e8c0c1a28b1e46b
collinginsburg <chginsburg@email.wm.edu>
2021-05-12 17:28:24 -0700

    calc number buttons now log to the screen, and the clear button clears the screen and the display variable

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ******************calc FUNCTIONS**************************************
+
 function subtraction (a,b){
     return a-b;
 }
@@ -27,3 +29,111 @@ function operate(operation,a,b){
     }
 
 }
+
+
+// ******************button FUNCTIONS**************************************
+const SCREEN =  document.querySelector('.screen');
+let displayvalue = SCREEN.textContent;
+
+function updateDisplay(){
+    displayvalue = SCREEN.textContent;
+};
+
+
+
+
+
+const ZERO = document.querySelector('.btn.zero');
+ZERO.addEventListener('click', displayZero);
+function displayZero(){
+    SCREEN.textContent += '0';
+    updateDisplay();
+    console.log(displayvalue);
+}
+
+const PERIOD = document.querySelector('.btn.period');
+PERIOD.addEventListener('click', displayPeriod);
+function displayPeriod(){
+    SCREEN.textContent += '.';
+    updateDisplay();
+    console.log(displayvalue);
+}
+
+const ONE = document.querySelector('.btn.one');
+ONE.addEventListener('click', displayOne);
+function displayOne(){
+    SCREEN.textContent += '1';
+    updateDisplay();
+    console.log(displayvalue);
+}
+const TWO = document.querySelector('.btn.two');
+TWO.addEventListener('click', displayTwo);
+function displayTwo(){
+    SCREEN.textContent += '2';
+    updateDisplay();
+    console.log(displayvalue);
+}
+const THREE = document.querySelector('.btn.three');
+THREE.addEventListener('click', displayThree);
+function displayThree(){
+    SCREEN.textContent += '3';
+    updateDisplay();
+    console.log(displayvalue);
+}
+const FOUR = document.querySelector('.btn.four');
+FOUR.addEventListener('click', displayFour);
+function displayFour(){
+    SCREEN.textContent += '4';
+    updateDisplay();
+    console.log(displayvalue);
+}
+const FIVE = document.querySelector('.btn.five');
+FIVE.addEventListener('click', displayFive);
+function displayFive(){
+    SCREEN.textContent += '5';
+    updateDisplay();
+    console.log(displayvalue);
+}
+const SIX = document.querySelector('.btn.six');
+SIX.addEventListener('click', displaySix);
+function displaySix(){
+    SCREEN.textContent += '6';
+    updateDisplay();
+    console.log(displayvalue);
+}
+const SEVEN = document.querySelector('.btn.seven');
+SEVEN.addEventListener('click', displaySeven);
+function displaySeven(){
+    SCREEN.textContent += '7';
+    updateDisplay();
+    console.log(displayvalue);
+}
+const EIGHT = document.querySelector('.btn.eight');
+EIGHT.addEventListener('click', displayEight);
+function displayEight(){
+    SCREEN.textContent += '8';
+    updateDisplay();
+    console.log(displayvalue);
+}
+const NINE = document.querySelector('.btn.nine');
+NINE.addEventListener('click', displayNine);
+function displayNine(){
+    SCREEN.textContent += '9';
+    updateDisplay();
+    console.log(displayvalue);
+}
+
+
+
+const CLEAR = document.querySelector('.btn.clear');
+CLEAR.addEventListener('click', clearDisplay);
+function clearDisplay(){
+    SCREEN.textContent = '';
+    updateDisplay();
+    console.log(displayvalue);
+};
+const DIVIDE = document.querySelector('.btn.divide');
+const MULTIPLY= document.querySelector('.btn.multiply');
+const MINUS = document.querySelector('.btn.minus');
+const PLUS = document.querySelector('.btn.plus');
+const EQUALS = document.querySelector('.btn.equals');
